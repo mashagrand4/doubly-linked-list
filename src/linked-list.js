afb73c1a192c453e800ab87272fc8ae02b9f4068
mashagrand4 <mashagrand4@gmail.com>
2017-01-28 05:28:20 +0300

    doubly-linked-list/feature: implementation methods in linked-list

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,19 +1,49 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this._tail = null;
+        this._head = null;
+        this.length = 0;
+    }
+    append(data) {
+        if(this._tail == null){
+            this._tail = new Node(data);
+            this._head = this._tail;
+        }else{
+            var temp = new Node(data);
+            this._tail.next = temp;
+            temp.prev = this._tail;
+            this._tail = temp;
+        }
+        this.length++;
+    }
+    head() {
+        return this._head.data;
+    }
 
-    append(data) {}
+    tail() {
+        return this._tail.data;
+    }
 
-    head() {}
-
-    tail() {}
-
-    at(index) {}
+    at(index) {
+        var tempNode = this._tail;
+        while (index < this.length-1){
+            tempNode = tempNode.prev;
+            index++;
+        }
+        return tempNode.data;
+    }
 
     insertAt(index, data) {}
 
-    isEmpty() {}
+    isEmpty() {
+        if (!this.length){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
     clear() {}
 
